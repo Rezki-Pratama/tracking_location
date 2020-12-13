4d800972b2455bb2b2431a311f1230198314a05e
Rezki-Pratama <rezki.pratama7777@gmail.com>
2020-12-13 18:26:51 +0700

    add gesture for get geo coordinates marker

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:here_sdk/mapview.dart';
+import 'package:tracking_location/gesture.dart';
 import 'package:tracking_location/main_page.dart';
 import 'package:here_sdk/core.dart';
 
 void main() {
   SdkContext.init(IsolateOrigin.main);
-  runApp(MyApp());
+  runApp(MaterialApp(home: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
+  BuildContext _context;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MainPage()
+      home: HereMap(onMapCreated: _onMapCreated)
     );
+  }
+
+  void _onMapCreated(HereMapController hereMapController) {
+    hereMapController.mapScene.loadSceneForMapScheme(MapScheme.normalDay,
+        (MapError error) {
+      if (error == null) {
+        GesturesExample(_context, hereMapController);
+      } else {
+        print("Map scene not loaded. MapError: " + error.toString());
+      }
+    });
   }
 }
